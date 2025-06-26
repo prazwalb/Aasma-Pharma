9acff25cca8e854f8c443cd42de624bfa33eeece
Prajwal Bastola <prazwal.bastola10@gmail.com>
2025-06-27 00:53:53 +0545

    refactorred

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:android_std/features/authentication/screens/onboarding.dart';
+import 'package:android_std/screens/onboarding.dart';
 import 'package:android_std/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
